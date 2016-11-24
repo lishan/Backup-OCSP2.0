adcb9cf7a5af8ed1b8bf195b1c031e42b203f787
wupeng <multiparallel@163.com>
2016-11-24 14:18:19 +0800

    Fix issue17
    Please check https://github.com/OCSP/OCSP_mainline/issues/17

--- build.sh
+++ build.sh
@@ -23,12 +23,13 @@ cp -r bin build/OCDP_Stream
 cp -r conf build/OCDP_Stream
 
 cp core/target/core-1.0-SNAPSHOT.jar build/OCDP_Stream/lib
-cp core/target/core-1.0-SNAPSHOT-dist/lib/jedis-2.6.1.jar build/OCDP_Stream/lib
-cp core/target/core-1.0-SNAPSHOT-dist/lib/commons-pool2-2.0.jar build/OCDP_Stream/lib
-cp core/target/core-1.0-SNAPSHOT-dist/lib/mysql-connector-java-5.1.34.jar build/OCDP_Stream/lib
-cp core/target/core-1.0-SNAPSHOT-dist/lib/spark-streaming-kafka-assembly_2.10-1.6.0.jar build/OCDP_Stream/lib
-cp core/target/core-1.0-SNAPSHOT-dist/lib/scala-library-2.10.4.jar build/OCDP_Stream/lib
-cp conf/common.xml build/OCDP_Stream/lib
+cp core/target/core-1.0-SNAPSHOT-dist/lib/jedis-*.jar build/OCDP_Stream/lib
+cp core/target/core-1.0-SNAPSHOT-dist/lib/jodis-*.jar build/OCDP_Stream/lib
+cp core/target/core-1.0-SNAPSHOT-dist/lib/commons-pool2-*.jar build/OCDP_Stream/lib
+cp core/target/core-1.0-SNAPSHOT-dist/lib/mysql-connector-java-*.jar build/OCDP_Stream/lib
+cp core/target/core-1.0-SNAPSHOT-dist/lib/spark-streaming-kafka-assembly_*.jar build/OCDP_Stream/lib
+cp core/target/core-1.0-SNAPSHOT-dist/lib/scala-library-*.jar build/OCDP_Stream/lib
+
 if [ -d "$PROJECT" ]; then
   cp ${PROJECT}/target/"$PROJECT"*.jar build/OCDP_Stream/lib
 else
