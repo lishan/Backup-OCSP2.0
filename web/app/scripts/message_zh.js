b0b73b5250b1018bc4533ca4885adc4c35883653
wupeng <multiparallel@163.com>
2016-12-22 15:10:19 +0800

    Update Chinese message

--- web/app/scripts/message_zh.js
+++ web/app/scripts/message_zh.js
@@ -3,28 +3,36 @@
  */
 angular.module('ocspApp').config(['$translateProvider', function($translateProvider) {
   $translateProvider.translations('zh', {
-    'task_manage': '任务管理',
-    'label_manage': '标签管理',
-    'system_manage': '系统管理',
-    'user_manage': '用户管理',
-    'create_task': '新建任务',
-    'create_task_1': '基础配置',
-    'create_task_2': '设置输入',
-    'create_task_3': '设置标签',
-    'create_task_4': '设置输出',
-    'create_task_5': '检查&提交',
-    'add_task': '新增任务',
-    'system_properties': '系统属性',
-    'current_password': '当前密码',
-    'new_password': '新密码',
-    'new_password_again': '再次输入',
-    'change_password': '修改密码',
-    'password_wrong': '密码错误，请重试。',
-    'username': '用户名',
-    'password': '密码',
-    'login': '登录',
-    'pass_not_same': '两次输入的密码不一致',
-    'timeh': '时',
-    'timem': '分'
+    'ocsp_web_common_000': '橘云流处理平台',
+    'ocsp_web_common_001': '用户名',
+    'ocsp_web_common_002': '密码',
+    'ocsp_web_common_003': '登录',
+    'ocsp_web_common_004': '退出',
+    'ocsp_web_common_005': '时',
+    'ocsp_web_common_006': '分',
+    'ocsp_web_common_007': '下一步',
+    'ocsp_web_common_008': '上一步',
+    'ocsp_web_common_009': '保存',
+    'ocsp_web_common_010': '上传',
+
+    'ocsp_web_user_manage_000':'用户管理',
+    'ocsp_web_user_manage_001':'修改密码',
+    'ocsp_web_user_manage_002':'当前密码',
+    'ocsp_web_user_manage_003':'新密码',
+    'ocsp_web_user_manage_004':'再次输入',
+    'ocsp_web_user_manage_005':'密码错误，请重试',
+    'ocsp_web_user_manage_006':'两次输入的密码不一致',
+
+    'ocsp_web_system_manage_000':'系统管理',
+    'ocsp_web_system_manage_001':'系统配置',
+    'ocsp_web_system_manage_001':'基础设置',
+
+    'ocsp_web_label_manage_000':'标签管理',
+    'ocsp_web_label_manage_001':'标签名字',
+    'ocsp_web_label_manage_002':'标签实现类',
+    'ocsp_web_label_manage_003':'标签参数',
+
+
+
   });
 }]);
