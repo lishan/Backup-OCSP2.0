e2daab55c08ea813ff613bb5626ad9389cbb2343
wupeng <multiparallel@163.com>
2016-12-22 16:04:44 +0800

    Update Chinese message

--- web/app/scripts/message_zh.js
+++ web/app/scripts/message_zh.js
@@ -14,6 +14,8 @@ angular.module('ocspApp').config(['$translateProvider', function($translateProvi
     'ocsp_web_common_008': '上一步',
     'ocsp_web_common_009': '保存',
     'ocsp_web_common_010': '上传',
+    'ocsp_web_common_011': '名字',
+    'ocsp_web_common_012': '描述',
 
     'ocsp_web_user_manage_000':'用户管理',
     'ocsp_web_user_manage_001':'修改密码',
@@ -32,7 +34,31 @@ angular.module('ocspApp').config(['$translateProvider', function($translateProvi
     'ocsp_web_label_manage_002':'标签实现类',
     'ocsp_web_label_manage_003':'标签参数',
 
-
+    'ocsp_web_streams_manage_000':'作业流管理',
+    'ocsp_web_streams_manage_001':'新建作业流',
+    'ocsp_web_streams_manage_002':'作业流概要',
+    'ocsp_web_streams_manage_003':'作业流配置',
+    'ocsp_web_streams_manage_004':'请先选择一个作业流',
+    'ocsp_web_streams_manage_005':'链接',
+    'ocsp_web_streams_manage_006':'基础配置',
+    'ocsp_web_streams_manage_007':'设置输入',
+    'ocsp_web_streams_manage_008':'设置标签',
+    'ocsp_web_streams_manage_009':'设置输出',
+    'ocsp_web_streams_manage_010':'检查&提交',
+    'ocsp_web_streams_manage_011':'输入流的名字',
+    'ocsp_web_streams_manage_012':'Kafka中topic的名字',
+    'ocsp_web_streams_manage_013':'输入流的字段',
+    'ocsp_web_streams_manage_014':'输入流字段分隔符',
+    'ocsp_web_streams_manage_015':'过滤条件',
+    'ocsp_web_streams_manage_016':'作为主键的字段',
+    'ocsp_web_streams_manage_017':'请选择',
+    'ocsp_web_streams_manage_018':'添加输出事件',
+    'ocsp_web_streams_manage_019':'事件名字',
+    'ocsp_web_streams_manage_020':'需要输出的字段',
+    'ocsp_web_streams_manage_021':'输出字段分隔符',
+    'ocsp_web_streams_manage_022':'输出事件的时间间隔',
+    'ocsp_web_streams_manage_023':'事件输出源',
+    'ocsp_web_streams_manage_024':'输出到Codis中Key的前缀'
 
   });
 }]);
