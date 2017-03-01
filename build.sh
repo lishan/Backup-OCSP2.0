047ea6fba5070495588ce656fb90e6ea6fb31dd0
wupeng <multiparallel@163.com>
2017-03-01 18:17:36 +0800

    支持spark2.1

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ version=2.0.1
 
 cd ${HOME_PATH}
 
-mvn clean package -Dmaven.test.skip=true
+mvn clean package -Dmaven.test.skip=true -Pspark-1.6
 
 if [ $? -ne 0 ]; then
    echo "Build failed..."
@@ -25,13 +25,7 @@ cp -r bin build/OCDP_Stream
 cp -r conf build/OCDP_Stream
 
 cp core/target/core-${version}.jar build/OCDP_Stream/lib
-cp core/target/core-${version}-dist/lib/jedis-*.jar build/OCDP_Stream/lib
-cp core/target/core-${version}-dist/lib/jodis-*.jar build/OCDP_Stream/lib
-cp core/target/core-${version}-dist/lib/commons-pool2-*.jar build/OCDP_Stream/lib
-cp core/target/core-${version}-dist/lib/mysql-connector-java-*.jar build/OCDP_Stream/lib
-cp core/target/core-${version}-dist/lib/spark-streaming-kafka-assembly_*.jar build/OCDP_Stream/lib
-cp core/target/core-${version}-dist/lib/scala-library-*.jar build/OCDP_Stream/lib
-cp core/target/core-${version}-dist/lib/spark-assembly-1.6.0.2.4.0.0-169-hadoop2.7.1.2.4.0.0-169*.jar build/OCDP_Stream/lib
+cp core/target/core-${version}-dist/lib/*.jar build/OCDP_Stream/lib
 
 tar -xzf web/target/web-${version}-bundle.tar.gz -C build/OCDP_Stream/web
 
---
+++ core/src/main/scala/com/asiainfo/ocdp/stream/spark2/manager/StreamApp.scala
@@ -0,0 +1,86 @@
+package com.asiainfo.ocdp.stream.spark2.manager
+
+import com.asiainfo.ocdp.stream.common.{Logging, SscManager}
+import com.asiainfo.ocdp.stream.config.MainFrameConf
+import com.asiainfo.ocdp.stream.constant.TaskConstant
+import com.asiainfo.ocdp.stream.manager.{AppStatusUpdateListener, TaskStopManager}
+import com.asiainfo.ocdp.stream.service.TaskServer
+import org.apache.spark.streaming.{Seconds, StreamingContext, StreamingContextState}
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by leo on 9/16/15.
+  */
+
+object StreamApp extends Logging {
+
+  val delaySeconds = MainFrameConf.systemProps.getInt("delaySeconds", 10)
+  val periodSeconds = MainFrameConf.systemProps.getInt("periodSeconds", 60)
+  val taskServer = new TaskServer
+
+  def main(args: Array[String]) {
+
+    if (args.length < 1) {
+      //      System.err.println("usage:  ./bin/spark-class com.asiainfo.ocdc.stream.mainframe [taskId]")
+      System.exit(1)
+    }
+
+    val taskId = args(0)
+
+    val taskConf = taskServer.getTaskInfoById(taskId)
+
+    //1 初始化 streamingContext
+
+    val sparkConf = new SparkConf().setAppName(taskConf.getName)
+    sparkConf.set("spark.scheduler.mode", "FAIR")
+
+    sparkConf.setAppName("OCDP_Streaming")
+
+    val sc = new SparkContext(sparkConf)
+    val listener = new AppStatusUpdateListener(taskConf.getId)
+    sc.addSparkListener(listener)
+
+    //2 启动 streamingContext
+    val ssc = new StreamingContext(sc, Seconds(taskConf.getReceive_interval))
+    //    ssc.addStreamingListener(new ReceiveRecordNumListener())
+    new TaskStopManager(ssc, taskConf.getId)
+
+    //将ssc存放到sscManager中
+    SscManager.initSsc(ssc)
+
+    try {
+      StreamTaskFactory.getStreamTask(taskConf).process(ssc)
+      ssc.start()
+      //4 update task status in db
+      if (StreamingContextState.ACTIVE == ssc.getState()) {
+        taskServer.startTask(taskConf.getId)
+        logInfo("Start task " + taskConf.getId + " sucess !")
+      }
+      ssc.awaitTermination()
+    } catch {
+      case e: Exception => {
+        e.printStackTrace()
+        if (taskServer.checkMaxRetry(taskConf.getId) > 0) {
+          logInfo("task : " + taskConf.getId + " got exception, task will retry")
+          taskServer.RetryTask(taskConf.getId)
+        }
+      }
+    } finally {
+//    ssc.awaitTermination()
+      ssc.stop()
+      //若task的状态是PRE_RESTART 则将数据库中的task status设为1,准备启动;
+      //否则将数据库中task status设为0,停止状态
+      if (TaskConstant.PRE_START == taskServer.checkTaskStatus(taskConf.getId) || TaskConstant.PRE_RESTART == taskServer.checkTaskStatus(taskConf.getId)){
+      taskServer.RestartTask(taskConf.getId)
+        logInfo("Restarting task " + taskConf.getId + " ...")
+      } else if (TaskConstant.RETRY != taskServer.checkTaskStatus(taskConf.getId)) {
+        taskServer.stopTask(taskConf.getId)
+        logInfo("Stop task " + taskConf.getId + " successfully !")
+      }
+
+      sys.exit()
+    }
+
+  }
+
+}
