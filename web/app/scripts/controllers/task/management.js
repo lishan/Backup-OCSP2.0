55dc92eec8cad6b6fcfcebc323b7a9d0f60f6e21
Yulishan <yulishan3@gmail.com>
2017-03-08 11:01:28 +0800

    #72 outputFields is based on input fields

--- web/app/scripts/controllers/task/management.js
+++ web/app/scripts/controllers/task/management.js
@@ -6,29 +6,22 @@
 angular.module('ocspApp')
   .controller('TaskManagementCtrl', ['$scope', '$http', 'Notification', '$q', 'usSpinnerService', 'loginService', '$interval', '$uibModal', '$filter', 'moment', function ($scope, $http, Notification, $q, usSpinnerService, loginService, $interval, $uibModal, $filter, moment) {
     loginService.init('task');
+    //i18n
     $scope.localLang = {
       search: $filter('translate')('ocsp_web_common_014'),
       nothingSelected : $filter('translate')('ocsp_web_common_017')
     };
-    $scope.selectedJob = {
-      input: {},
-      events: []
-    };
-    $scope.actions = [
-      {name: $filter('translate')('ocsp_web_streams_manage_024'), enable: true, icon : "glyphicon glyphicon-play success"},
-      {name: $filter('translate')('ocsp_web_streams_manage_025'), enable: true, icon: "glyphicon glyphicon-stop danger"},
-      {name: $filter('translate')('ocsp_web_streams_manage_026'), enable: true, icon: "glyphicon glyphicon-refresh danger"},
-      {name: $filter('translate')('ocsp_web_streams_manage_027'), enable: true, icon: "glyphicon glyphicon-remove-sign warning"}
-    ];
+    //Initialize charts and links
     mermaidAPI.initialize({
       startOnLoad:false
     });
-    $http.get("/api/prop/spark").success(function(result){
-      if(result === null || result === "" || result.value === ""){
-        $scope.openSparkModal();
-      }
+    $scope.links = [];
+    $http.get('/api/config/links').success(function(data){
+      $scope.links = data;
     });
-    $scope.openSparkModal = function(){
+
+    //Check spark_home properties
+    function _openSparkModal(){
       var modal = $uibModal.open({
         animation: true,
         ariaLabelledBy: 'modal-title-bottom',
@@ -40,19 +33,33 @@ angular.module('ocspApp')
           $scope.inputSpark = function(){
             $http.post("/api/prop/spark", {spark: $scope.spark}).success(function(){
               modal.close();
-              $scope.showModal();
+              $scope._showModal();
             }).error(function(err){
               Notification.error(err);
             });
           };
         }]
       });
-    };
-    $scope.showModal = function(){
+    }
+    $http.get("/api/prop/spark").success(function(result){
+      if(result === null || result === "" || result.value === ""){
+        _openSparkModal();
+      }
+    });
+
+    $scope._showModal = function(){
       $scope.$broadcast('openModal',{});
     };
 
-    function dealWith(status) {
+    //Actions and change
+    $scope.actions = [
+      {name: $filter('translate')('ocsp_web_streams_manage_024'), enable: true, icon : "glyphicon glyphicon-play success"},
+      {name: $filter('translate')('ocsp_web_streams_manage_025'), enable: true, icon: "glyphicon glyphicon-stop danger"},
+      {name: $filter('translate')('ocsp_web_streams_manage_026'), enable: true, icon: "glyphicon glyphicon-refresh danger"},
+      {name: $filter('translate')('ocsp_web_streams_manage_027'), enable: true, icon: "glyphicon glyphicon-remove-sign warning"}
+    ];
+
+    function _dealWith(status) {
       if(status === 0){
         $scope.actions = [
           {name: $filter('translate')('ocsp_web_streams_manage_024'), enable: true, icon : "glyphicon glyphicon-play success"},
@@ -84,35 +91,116 @@ angular.module('ocspApp')
       }
     }
 
-    var taskInterval = $interval(function () {
-      $http.get('/api/task/status').success(function(tasks){
-        if($scope.jobs !== undefined && $scope.jobs.length > 0){
-          for(let i in $scope.jobs){
-            for(let j in tasks){
-              if($scope.jobs[i].id === tasks[j].id){
-                $scope.jobs[i].status = tasks[j].status;
-                $scope.jobs[i].running_time = tasks[j].running_time;
-                break;
-              }
-            }
+    $scope.statusName = function(item){
+      switch(item){
+        case 0: return "glyphicon glyphicon-warning-sign danger"; //stop
+        case 1: return "glyphicon glyphicon-ok-sign success animated flash infinite"; // pre_start
+        case 2: return "glyphicon glyphicon-ok-sign success"; // running
+        case 3: return "glyphicon glyphicon-warning-sign danger animated flash infinite"; // pre_stop
+        case 4: return "glyphicon glyphicon-ok-sign success animated flash infinite"; // pre_restart
+        case 5: return "glyphicon glyphicon-refresh warning animated flash infinite"; // retry
+      }
+    };
+
+    $scope.statusText = function(item){
+      switch(item) {
+        case 0:
+          return $filter('translate')('ocsp_web_streams_manage_032');
+        case 1:
+          return $filter('translate')('ocsp_web_streams_manage_033');
+        case 2:
+          return $filter('translate')('ocsp_web_streams_manage_034');
+        case 3:
+          return $filter('translate')('ocsp_web_streams_manage_035');
+        case 4:
+          return $filter('translate')('ocsp_web_streams_manage_036');
+        case 5:
+          return $filter('translate')('ocsp_web_streams_manage_044');
+      }
+    };
+
+    $scope.changeStatus = function(item){
+      var name = item.name;
+      if(!item.enable){
+        return;
+      }
+      var status = 0;
+      if(name === $filter('translate')('ocsp_web_streams_manage_024')){
+        status = 1;
+      }else if(name === $filter('translate')('ocsp_web_streams_manage_025')){
+        status = 3;
+      }else if(name === $filter('translate')('ocsp_web_streams_manage_026')){
+        status = 4;
+      }else if(name === $filter('translate')('ocsp_web_streams_manage_027')){
+        if(confirm("Are you sure?")){
+          if($scope.selectedJob.id === undefined || $scope.selectedJob.id === null){
+            Notification.error("Cannot delete null task");
+          }else{
+            usSpinnerService.spin('spinner');
+            $http.post("/api/task/delete/" + $scope.selectedJob.id, {type: 0}).success(function(){
+              $scope.selectedJob.type = 0;
+              Notification.success($filter('translate')('ocsp_web_common_029'));
+              usSpinnerService.stop('spinner');
+            }).error(function(err){
+              usSpinnerService.stop('spinner');
+              Notification.error(err);
+            });
           }
         }
-        dealWith($scope.selectedJob.status);
-      });
-    }, 3000);
-    $scope.$on('$destroy', function(){
-      if(taskInterval) {
-        $interval.cancel(taskInterval);
+        return;
       }
-    });
-    $scope.links = [];
-    $http.get('/api/config/links').success(function(data){
-      $scope.links = data;
-    });
+      if($scope.selectedJob.id === undefined || $scope.selectedJob.id === null){
+        Notification.error("Cannot update null task");
+      }else{
+        usSpinnerService.spin('spinner');
+        $http.post("/api/task/change/" + $scope.selectedJob.id, {status: status}).success(function(){
+          $scope.selectedJob.status = status;
+          _dealWith(status);
+          Notification.success($filter('translate')('ocsp_web_common_026'));
+          usSpinnerService.stop('spinner');
+        }).error(function(err){
+          usSpinnerService.stop('spinner');
+          Notification.error(err);
+        });
+      }
+    };
+
+    // Globale timer
+    // var taskInterval = $interval(function () {
+    //   $http.get('/api/task/status').success(function(tasks){
+    //     if($scope.jobs !== undefined && $scope.jobs.length > 0){
+    //       for(let i in $scope.jobs){
+    //         for(let j in tasks){
+    //           if($scope.jobs[i].id === tasks[j].id){
+    //             $scope.jobs[i].status = tasks[j].status;
+    //             $scope.jobs[i].running_time = tasks[j].running_time;
+    //             break;
+    //           }
+    //         }
+    //       }
+    //     }
+    //     _dealWith($scope.selectedJob.status);
+    //   });
+    // }, 3000);
+    // $scope.$on('$destroy', function(){
+    //   if(taskInterval) {
+    //     $interval.cancel(taskInterval);
+    //   }
+    // });
+
+    $scope.selectedJob = {
+      input: {
+        inputs: []
+      },
+      events: []
+    };
+
     function init() {
       // Use for create new task
       $scope.task = {
-        input: {},
+        input: {
+          inputs: []
+        },
         events: []
       };
       usSpinnerService.spin('spinner');
@@ -135,24 +223,16 @@ angular.module('ocspApp')
 
     init();
 
-    $scope.onSelect = function(item){
-      //Clear periods when select audit type
-      item.audit.periods = [{}];
-    };
-
-    $scope.addPeriod = function(item){
-      item.audit.periods.push({});
-    };
-
-    $scope.removePeriod = function(item, $index){
-      item.audit.periods.splice($index,1);
-    };
-
     $scope.update = function(){
       if($scope.selectedJob.id === undefined || $scope.selectedJob.id === null){
         Notification.error("Cannot update null task");
       }else{
-        if($("#mainFrame .ng-invalid").length > 0){
+        if ($scope.mainForm.$invalid) {
+          angular.forEach($scope.mainForm.$error, function (field) {
+            angular.forEach(field, function(errorField){
+              errorField.$setTouched();
+            });
+          });
           Notification.error($filter('translate')('ocsp_web_common_032'));
         }else {
           usSpinnerService.spin('spinner');
@@ -168,10 +248,11 @@ angular.module('ocspApp')
     };
 
     //Basic functions for page operation
-    var parseProperties = function (datainterface, prop){
+    var _parseProperties = function (datainterface, prop, type = "output"){
       if(datainterface.delim === "\\|"){
         datainterface.delim = "|";
       }
+      datainterface.inputs = [];
       if(prop !== undefined && prop !== null) {
         prop = JSON.parse(prop);
         if(prop.fields !== undefined && prop.fields.length > 0) {
@@ -198,10 +279,39 @@ angular.module('ocspApp')
             }
           }
         }
+        if(prop.sources !== undefined && prop.sources.length > 0 && type === "input") {
+          for(let i = 0 ; i < prop.sources.length; i++){
+            let result = {
+              name: prop.sources[i].pname,
+              delim: prop.sources[i].delim === "\\|"?"|":prop.sources[i].delim,
+              fields: "",
+              userFields: []
+            };
+            if(prop.sources[i].fields !== undefined && prop.sources[i].fields.length > 0) {
+              if (prop.sources[i].fields.length > 0){
+                result.fields = prop.sources[i].fields[0].pname;
+              }
+              for (let j = 1; j < prop.sources[i].fields.length; j++) {
+                if(prop.sources[i].fields[j].pname !== undefined && prop.sources[i].fields[j].pname !== "") {
+                  result.fields += "," + prop.sources[i].fields[j].pname;
+                }
+              }
+            }
+            if(prop.sources[i].userFields !== undefined && prop.sources[i].userFields.length > 0) {
+              for (let j = 0; j < prop.sources[i].userFields.length; j++) {
+                result.userFields.push({
+                  name: prop.sources[i].userFields[j].pname,
+                  value: prop.sources[i].userFields[j].pvalue
+                });
+              }
+            }
+            datainterface.inputs.push(result);
+          }
+        }
       }
     };
 
-    function drawGraph(item, labels){
+    function _drawGraph(item, labels){
       var graphDefinition = 'graph LR;';
       graphDefinition += "task[" + item.name + "];";
       graphDefinition += "input((" + item.input.topic + "));";
@@ -250,7 +360,7 @@ angular.module('ocspApp')
       mermaidAPI.render('graphDiv', graphDefinition, insertSvg);
     }
 
-    var parseDatasource = function (dataInterface){
+    var _parseDatasource = function (dataInterface){
       for(var i in $scope.datasources){
         if($scope.datasources[i].id === dataInterface.dsid){
           dataInterface.datasource = $scope.datasources[i];
@@ -273,8 +383,8 @@ angular.module('ocspApp')
           $scope.selectedJob.events = arr.events.data;
           var labels = [];
           //Deal with input properties
-          parseDatasource($scope.selectedJob.input);
-          parseProperties($scope.selectedJob.input, $scope.selectedJob.input.properties);
+          _parseDatasource($scope.selectedJob.input);
+          _parseProperties($scope.selectedJob.input, $scope.selectedJob.input.properties, "input");
           //Deal with labels
           for (let j in $scope.inputLabels) {
             $scope.inputLabels[j].tick2 = false;
@@ -348,139 +458,21 @@ angular.module('ocspApp')
                 for(let j in $scope.selectedJob.output){
                   if($scope.selectedJob.output[j].id === parseInt($scope.selectedJob.events[i].PROPERTIES.output_dis[0].diid)){
                     $scope.selectedJob.events[i].output = $scope.selectedJob.output[j];
-                    parseProperties($scope.selectedJob.events[i].output, $scope.selectedJob.events[i].output.properties);
-                    parseDatasource($scope.selectedJob.events[i].output);
+                    _parseProperties($scope.selectedJob.events[i].output, $scope.selectedJob.events[i].output.properties);
+                    _parseDatasource($scope.selectedJob.events[i].output);
                     break;
                   }
                 }
               }
             }
           }
-          dealWith($scope.selectedJob.status);
-          drawGraph($scope.selectedJob, labels);
+          _dealWith($scope.selectedJob.status);
+          _drawGraph($scope.selectedJob, labels);
           usSpinnerService.stop('spinner');
         }, function(err){
           Notification.error(err.data);
           usSpinnerService.stop('spinner');
         });
-    };
-
-    $scope.statusName = function(item){
-      switch(item){
-        case 0: return "glyphicon glyphicon-warning-sign danger"; //stop
-        case 1: return "glyphicon glyphicon-ok-sign success animated flash infinite"; // pre_start
-        case 2: return "glyphicon glyphicon-ok-sign success"; // running
-        case 3: return "glyphicon glyphicon-warning-sign danger animated flash infinite"; // pre_stop
-        case 4: return "glyphicon glyphicon-ok-sign success animated flash infinite"; // pre_restart
-        case 5: return "glyphicon glyphicon-refresh warning animated flash infinite"; // retry
-      }
-    };
-
-    $scope.statusText = function(item){
-      switch(item) {
-        case 0:
-          return $filter('translate')('ocsp_web_streams_manage_032');
-        case 1:
-          return $filter('translate')('ocsp_web_streams_manage_033');
-        case 2:
-          return $filter('translate')('ocsp_web_streams_manage_034');
-        case 3:
-          return $filter('translate')('ocsp_web_streams_manage_035');
-        case 4:
-          return $filter('translate')('ocsp_web_streams_manage_036');
-        case 5:
-          return $filter('translate')('ocsp_web_streams_manage_044');
-      }
-    };
-
-    $scope.changeStatus = function(item){
-      var name = item.name;
-      if(!item.enable){
-        return;
-      }
-      var status = 0;
-      if(name === $filter('translate')('ocsp_web_streams_manage_024')){
-        status = 1;
-      }else if(name === $filter('translate')('ocsp_web_streams_manage_025')){
-        status = 3;
-      }else if(name === $filter('translate')('ocsp_web_streams_manage_026')){
-        status = 4;
-      }else if(name === $filter('translate')('ocsp_web_streams_manage_027')){
-        if(confirm("Are you sure?")){
-          if($scope.selectedJob.id === undefined || $scope.selectedJob.id === null){
-            Notification.error("Cannot delete null task");
-          }else{
-            usSpinnerService.spin('spinner');
-            $http.post("/api/task/delete/" + $scope.selectedJob.id, {type: 0}).success(function(){
-              $scope.selectedJob.type = 0;
-              Notification.success($filter('translate')('ocsp_web_common_029'));
-              usSpinnerService.stop('spinner');
-            }).error(function(err){
-              usSpinnerService.stop('spinner');
-              Notification.error(err);
-            });
-          }
-        }
-        return;
-      }
-      if($scope.selectedJob.id === undefined || $scope.selectedJob.id === null){
-        Notification.error("Cannot update null task");
-      }else{
-        usSpinnerService.spin('spinner');
-        $http.post("/api/task/change/" + $scope.selectedJob.id, {status: status}).success(function(){
-          $scope.selectedJob.status = status;
-          dealWith(status);
-          Notification.success($filter('translate')('ocsp_web_common_026'));
-          usSpinnerService.stop('spinner');
-        }).error(function(err){
-          usSpinnerService.stop('spinner');
-          Notification.error(err);
-        });
-      }
-    };
-
-    $scope.tab = "summary";
-    $scope.changeTab = function(name){
-      $scope.tab = name;
-    };
-
-    $scope.removeOutput = function($index){
-      $scope.task.events.splice($index,1);
-    };
-
-    $scope.removeOutput1 = function($index){
-      $scope.selectedJob.events.splice($index,1);
-    };
-
-    $scope.addOutput = function () {
-      $scope.task.events.push({
-        output:{}
-      });
-    };
-
-    $scope.addOutput1 = function () {
-      $scope.selectedJob.events.push({
-        status: 1,
-        output:{}
-      });
-    };
-
-    $scope.sortLabels = function(arr, index){
-      var temp = $scope.inputLabels;
-      arr.splice(index, 1);
-      $scope.inputLabels = arr;
-      for(let i in temp){
-        var flag = true;
-        for(let j in arr){
-          if(temp[i].id === arr[j].id){
-            flag = false;
-            break;
-          }
-        }
-        if(flag){
-          $scope.inputLabels.push(temp[i]);
-        }
-      }
     };
 
     $scope.submitMethod = function(){
@@ -502,6 +494,50 @@ angular.module('ocspApp')
         });
       }
       return defer.promise;
+    };
+
+    //Page helpers
+
+    $scope.tab = "summary";
+    $scope.changeTab = function(name){
+      $scope.tab = name;
+    };
+
+    $scope.remove = function(array, $index){
+      array.splice($index,1);
+    };
+
+    $scope.add = function (array) {
+      if(array !== undefined) {
+        array.push({
+          status: 1,
+          output: {},
+          userFields: []
+        });
+      }
+    };
+
+    $scope.onSelect = function(item){
+      //Clear periods when select audit type
+      item.audit.periods = [{}];
+    };
+
+    $scope.sortLabels = function(arr, index){
+      var temp = $scope.inputLabels;
+      arr.splice(index, 1);
+      $scope.inputLabels = arr;
+      for(let i in temp){
+        var flag = true;
+        for(let j in arr){
+          if(temp[i].id === arr[j].id){
+            flag = false;
+            break;
+          }
+        }
+        if(flag){
+          $scope.inputLabels.push(temp[i]);
+        }
+      }
     };
 
   }]);
--- web/app/scripts/directive/tokenfield.js
+++ web/app/scripts/directive/tokenfield.js
@@ -9,12 +9,61 @@ angular.module('ocspApp').directive('tokenfield',[function() {
     transclude: true,
     replace: true,
     scope: {
-      ngModel: '='
+      ngModel: '=',
+      autoCompleteModel: "="
     },
-    link: function (scope, element) {
-      var $e = element.find('input');
-      var token = $e.tokenfield({
-        sortable: true
+    link: function (scope, element, attrs) {
+      let _findLabelTips = function(array, labels){
+        let result = new Set();
+        if(array !== undefined && array.length > 0){
+          result = new Set(array.split(","));
+        }
+        if(labels !== undefined && labels !== ""){
+          labels = JSON.parse(labels);
+          for(let i in labels){
+            if(labels[i].properties !== undefined) {
+              let items = JSON.parse(labels[i].properties);
+              for (let j in items.labelItems) {
+                if(!result.has(items.labelItems[j].pvalue)) {
+                  result.add(items.labelItems[j].pvalue);
+                }
+              }
+            }
+          }
+        }
+        return [...result];
+      };
+      scope.bRequired = attrs !== undefined && attrs.required === true ? true : false;
+      let $e = element.find('input');
+      let token = {};
+      // Add tips
+      if(scope.autoCompleteModel !== undefined) {
+        token = $e.tokenfield({
+          autocomplete: {
+            source: scope.autoCompleteModel.split(','),
+            delay: 100
+          },
+          sortable: true,
+          showAutocompleteOnFocus: true
+        });
+      }else{
+        token = $e.tokenfield({
+          sortable: true,
+        });
+      }
+      attrs.$observe('labels', function(labels) {
+        if(labels !== undefined && labels !== null){
+          $e.data('bs.tokenfield').$input.autocomplete({source: _findLabelTips(scope.autoCompleteModel,labels)});
+        }
+      });
+      //Disable duplicated keys
+      $e.on('tokenfield:createtoken', function (event) {
+        var existingTokens = $(this).tokenfield('getTokens');
+        $.each(existingTokens, function(index, token) {
+          if (token.value === event.attrs.value) {
+            event.preventDefault();
+          }
+        });
       });
       scope.$watch('ngModel', function() {
         token.tokenfield('setTokens', scope.ngModel);
