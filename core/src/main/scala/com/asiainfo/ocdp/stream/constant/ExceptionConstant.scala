f332aa21011c02d38a38647100f3a84e1627e791
rainday <raindaywhu@163.com>
2017-03-17 09:59:39 +0800

    add Exception define

---
+++ core/src/main/scala/com/asiainfo/ocdp/stream/constant/ExceptionConstant.scala
@@ -0,0 +1,27 @@
+package com.asiainfo.ocdp.stream.constant
+
+import scala.collection.mutable
+
+/**
+  * Created by rainday on 3/8/17.
+  */
+object ExceptionConstant {
+
+  val EXCEPTION_WARNING = 1
+  val EXCEPTION_ERROR = 2
+  val EXCEPTION_FATAL = 3
+
+  val ERR_NONE = 0
+  val ERR_SPARK_JOB_FINISHED = 1
+  val ERR_JOB_EXCEPTION = 2
+
+  val EXCEPTION_CODE = mutable.Map[Int, String] (
+    ERR_NONE -> "no exception",
+    ERR_SPARK_JOB_FINISHED -> "ERROR! This spark app already exited!",
+    ERR_JOB_EXCEPTION -> "ERROR! This stream had something wrong and exited after exception !"
+  )
+
+  def getExceptionInfo(exception_code: Int): String = {
+    EXCEPTION_CODE.getOrElse(exception_code, "no exception")
+  }
+}
