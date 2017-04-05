ed8a0f094a893b88956494c7bd32f1d23e6637e1
yuls3 <yulishan3@gmail.com>
2017-04-05 14:56:17 +0800

    #96, now we support sha-256 password encrypt

--- web/server/api/user.js
+++ web/server/api/user.js
@@ -5,11 +5,10 @@ let sequelize = require('../sequelize');
 let Sequelize = require('sequelize');
 let User = require('../model/STREAM_USER')(sequelize, Sequelize);
 let exec = require('child_process').exec;
-
 let router = express.Router();
+const prefix = "./server/lib/";
 
 router.post('/login', function (req, res) {
-  const prefix = "./server/lib/";
   let username = req.body.username;
   let pass = req.body.password;
   exec(`java -Dconfig=${prefix}${shiroConfig} -Dtype=encrypt -Dusername=${username} -Dpassword=${pass} -jar ${prefix}${jarPack}`,
@@ -41,7 +40,7 @@ router.post('/login', function (req, res) {
 router.get('/', function(req, res){
   let usertype = req.query.usertype;
   if(usertype === "admin") {
-    User.findAll().then(function (users) {
+    User.findAll({attributes: ['id', 'name', 'description']}).then(function (users) {
       res.send(users);
     }, function () {
       res.status(500).send(trans.databaseError);
@@ -54,15 +53,34 @@ router.get('/', function(req, res){
 router.put('/', function(req, res){
   let users = req.body.users;
   let usertype = req.query.usertype;
+  let _parsePassword = function (promises, i) {
+    promises.push(new sequelize.Promise((resolve, reject) => {
+      let user = users[i];
+      if(user.password !== undefined && user.password !== null) {
+        exec(`LC_ALL=en java -Dtype=sha-256 -Dusername=${user.name} -Dpassword=${user.password} -jar ${prefix}${jarPack}`,
+          (error, password) => {
+            if (error === null) {
+              user.password = password.trim();
+              if (users[i].id !== undefined && users.id !== null) {
+                promises.push(User.update(users[i], {where: {id: users[i].id}}));
+              } else {
+                promises.push(User.create(users[i]));
+              }
+              resolve();
+            } else {
+              reject(trans.authError);
+            }
+          });
+      }else{
+        resolve();
+      }
+    }));
+  };
   if(usertype === "admin") {
     User.findAll().then((dbUsers) => {
       let promises = [];
       for (let i in users) {
-        if (users[i].id !== undefined && users.id !== null) {
-          promises.push(User.update(users[i], {where: {id: users[i].id}}));
-        } else {
-          promises.push(User.create(users[i]));
-        }
+        _parsePassword(promises, i);
       }
       for(let i in dbUsers){
         let flag = true;
@@ -94,14 +112,21 @@ router.post('/change', function(req, res){
   let usertype = req.query.usertype;
   let user = req.body.user;
   if(username === user.name || usertype === "admin") {
-    let pass = user.oldPassword;
-    User.find({where: {name: user.name, password: pass}}).then(function (data) {
+    User.find({where: {name: user.name}}).then(function (data) {
       if (data !== null && data !== undefined && data.dataValues !== undefined) {
-        User.update(user, {where: {id: data.dataValues.id}}).then(function () {
-          res.send({status: true});
-        }, function () {
-          res.send({status: false});
-        });
+        exec(`LC_ALL=en java -Dtype=sha-256 -Dusername=${user.name} -Dpassword=${user.password} -jar ${prefix}${jarPack}`,
+          (error, password) => {
+            if(error === null){
+              user.password = password.trim();
+              User.update(user, {where: {id: data.dataValues.id}}).then(function () {
+                res.send({status: true});
+              }, function () {
+                res.send({status: false});
+              });
+            }else{
+              res.status(500).send(trans.authError);
+            }
+          });
       } else {
         res.send({status: false});
       }
