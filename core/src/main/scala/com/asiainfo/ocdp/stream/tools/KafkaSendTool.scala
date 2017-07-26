23a4dab79ef500befdf1ab1b87b6dc15fc476e97
GalvinYang <yangguangmingzi@126.com>
2017-07-26 12:39:22 +0800

    enable kerberos on output to kafka
    把以前用的kafka0.8的api换成新的kafka0.10的api

--- core/src/main/scala/com/asiainfo/ocdp/stream/tools/KafkaSendTool.scala
+++ core/src/main/scala/com/asiainfo/ocdp/stream/tools/KafkaSendTool.scala
@@ -3,8 +3,8 @@ package com.asiainfo.ocdp.stream.tools
 import java.util.Properties
 
 import com.asiainfo.ocdp.stream.config.{ DataSourceConf, MainFrameConf }
-import kafka.producer.{ KeyedMessage, ProducerConfig, Producer }
-
+//import kafka.producer.{ KeyedMessage, ProducerConfig, Producer }
+import org.apache.kafka.clients.producer.{KafkaProducer, Producer, ProducerConfig, ProducerRecord}
 import scala.collection.mutable
 
 /**
@@ -21,7 +21,7 @@ object KafkaSendTool {
   val DEFAULT_SERIALIZER_CLASS = "kafka.serializer.StringEncoder"
 
   // surq:一个datasource 对应一个producer，跟业务个数无直接关系
-  val dsid2ProducerMap = mutable.Map[String, Producer[String, String]]()
+  val dsid2ProducerMap = mutable.Map[String, KafkaProducer[String, String]]()
 
   // 多线程、多producer,分包发送
   //  def sendMessage(dsConf: DataSourceConf, message: List[KeyedMessage[String, String]]) =
@@ -29,21 +29,28 @@ object KafkaSendTool {
   //      override def run() = getProducer(dsConf).send(list: _*)
   //    }))
 
-  def sendMessage(dsConf: DataSourceConf, message: List[KeyedMessage[String, String]]) = {
-    val msgList = message.sliding(200, 200)
+  def sendMessage(dsConf: DataSourceConf, message: List[ProducerRecord[String, String]]) = {
+    val msgList: Iterator[List[ProducerRecord[String, String]]] = message.sliding(200, 200)
     if (msgList.size > 0){
-    	val producer = getProducer(dsConf)
-    	message.sliding(200, 200).foreach(list => producer.send(list: _*))
+    	val producer: KafkaProducer[String, String] = getProducer(dsConf)
+    	message.sliding(200, 200).foreach((list: List[ProducerRecord[String, String]]) => {
+        list.foreach((record: ProducerRecord[String, String]) => {
+          producer.send(record)
+        })
+    })
     }
   }
 
   // 对应的producer若不存在，则创建新的producer，并存入dsid2ProducerMap
-  private def getProducer(dsConf: DataSourceConf): Producer[String, String] =
+  private def getProducer(dsConf: DataSourceConf): KafkaProducer[String, String] =
     dsid2ProducerMap.getOrElseUpdate(dsConf.dsid, {
       val props = new Properties()
-      props.put("metadata.broker.list", dsConf.get("metadata.broker.list", ""))
-      props.put("serializer.class", dsConf.get("serializer.class", DEFAULT_SERIALIZER_CLASS))
-      new Producer[String, String](new ProducerConfig(props))
+      props.put("bootstrap.servers", dsConf.get("metadata.broker.list", ""))
+      props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+      props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+      props.put("security.protocol","SASL_PLAINTEXT")
+      //props.put("serializer.class", dsConf.get("serializer.class", DEFAULT_SERIALIZER_CLASS))
+      new KafkaProducer[String, String](props)
     })
 
 }
